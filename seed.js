a936129fe00baf611ed98b01e996e36750a2adef
Jari1980 <jari.tenhunen@outlook.com>
2026-03-17 16:53:31 +0100

    album seed

---
+++ seed.js
@@ -0,0 +1,18 @@
+const Album = require("./models/Album");
+
+async function seedAlbums(){
+    const count = await Album.countDocuments();
+    if(count === 0){
+        console.log("Seeding database...")
+        await Album.insertMany([
+            {title: "Broccoli", artist: "SuperMan", year: 1980},
+            {title: "GreenIsGood", artist: "Another Bites Dust", year: 1990},
+            {title: "What is Java", artist: "Gevalia", year: 2000}
+        ]);
+        console.log("Seeding completed");
+    } else {
+        console.log("Not seeding since there are albums in database");
+    }
+}
+
+module.exports = seedAlbums;
